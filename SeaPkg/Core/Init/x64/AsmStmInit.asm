b0585edd7ec869d21a8f86cb294499f6369f2f46
Michael Kubacki <michael.kubacki@microsoft.com>
2024-10-22 11:24:10 -0400

    AsmStmInit.asm: Add constant stack algorithm commented out

--- SeaPkg/Core/Init/x64/AsmStmInit.asm
+++ SeaPkg/Core/Init/x64/AsmStmInit.asm
@@ -23,7 +23,7 @@ externdef _ModuleEntryPoint:NEAR
 SEA_API_GET_CAPABILITIES  EQU 00010101h
 SEA_API_GET_RESOURCES     EQU 00010102h
 
-STM_STACK_SIZE                EQU 08000h
+STM_STACK_SIZE            EQU 08000h
 
 ;------------------------------------------------------------------------------
 ; VOID
@@ -38,12 +38,44 @@ _ModuleEntryPoint PROC PUBLIC
   jmp DeadLoop
 
 GoGetCapabilities:
+;; NEW:
+; |===================|<-ESP (Proc[N])
+; | Proc[N] Reg       |
+; |-------------------|
+; | Proc[N] Stack     |
+; |===================|<-ESP (Proc[N-1])
+; | Proc[N-1] Reg     |
+; |-------------------|
+; | Proc[N-1] Stack   |
+; |===================|
+; | ...               |
+; |===================|<-ESP (Proc[0])
+; | Proc[0] Reg       |
+; |-------------------|
+; | Proc[0] Stack     |
+; |===================|
+; | ThisOffset        |
+; +===================+<-ESP (Common)
+; | Heap              |
+; |===================|
+
   ; Assume ThisOffset is 0
   ; ESP is pointer to stack bottom, NOT top
   mov  eax, STM_STACK_SIZE     ; eax = STM_STACK_SIZE,
   lock xadd [esp], eax         ; eax = ThisOffset, ThisOffset += STM_STACK_SIZE (LOCK instruction)
   add  eax, STM_STACK_SIZE     ; eax = ThisOffset + STM_STACK_SIZE
   add  esp, eax                ; esp += ThisOffset + STM_STACK_SIZE
+
+  ; ; ESP is pointer to stack bottom, NOT top
+  ; ; Get the APIC ID
+  ; mov eax, 0xFEE00020                    ; APIC Base = 0xFEE00000 (APIC ID 0x20)
+  ; mov eax, [eax]                         ; Read the APIC ID [31:24]
+  ; shr eax, 24                            ; Get APIC ID in lower 8 bits
+
+  ; ; Calculate stack address based on APIC ID
+  ; imul eax, STM_STACK_SIZE               ; eax = APIC_ID * STM_STACK_SIZE
+  ; add eax, STM_STACK_SIZE                ; eax += STM_STACK_SIZE (stack grows down)
+  ; add esp, eax                           ; esp += stack area base address (ESP common) + (STM_STACK_SIZE * StackOffset)
 
   ;
   ; Jump to C code
@@ -61,7 +93,7 @@ GoGetCapabilities:
   push rdi
   push rsi
   push rbp
-  push rsp ; should be rsp
+  push rbp ; should be rsp
   push rbx
   push rdx
   push rcx
@@ -78,6 +110,8 @@ GoGetResources:
 ;
 ; assign unique ESP for each processor
 ;
+
+;; OLD:
 ; |------------|<-ESP (PerProc)
 ; | Reg        |
 ; |------------|
@@ -87,12 +121,44 @@ GoGetResources:
 ; +------------+<-ESP (Common)
 ; | Heap       |
 
+;; NEW:
+; |===================|<-ESP (Proc[N])
+; | Proc[N] Reg       |
+; |-------------------|
+; | Proc[N] Stack     |
+; |===================|<-ESP (Proc[N-1])
+; | Proc[N-1] Reg     |
+; |-------------------|
+; | Proc[N-1] Stack   |
+; |===================|
+; | ...               |
+; |===================|<-ESP (Proc[0])
+; | Proc[0] Reg       |
+; |-------------------|
+; | Proc[0] Stack     |
+; |===================|
+; | ThisOffset        |
+; +===================+<-ESP (Common)
+; | Heap              |
+; |===================|
+
   ; Assume ThisOffset is 0
   ; ESP is pointer to stack bottom, NOT top
   mov  eax, STM_STACK_SIZE     ; eax = STM_STACK_SIZE,
   lock xadd [esp], eax         ; eax = ThisOffset, ThisOffset += STM_STACK_SIZE (LOCK instruction)
   add  eax, STM_STACK_SIZE     ; eax = ThisOffset + STM_STACK_SIZE
   add  esp, eax                ; esp += ThisOffset + STM_STACK_SIZE
+
+  ; ; ESP is pointer to stack bottom, NOT top
+  ; ; Get the APIC ID
+  ; mov eax, 0xFEE00020                    ; APIC Base = 0xFEE00000 (APIC ID 0x20)
+  ; mov eax, [eax]                         ; Read the APIC ID [31:24]
+  ; shr eax, 24                            ; Get APIC ID in lower 8 bits
+
+  ; ; Calculate stack address based on APIC ID
+  ; imul eax, STM_STACK_SIZE               ; eax = APIC_ID * STM_STACK_SIZE
+  ; add eax, STM_STACK_SIZE                ; eax += STM_STACK_SIZE (stack grows down)
+  ; add esp, eax                           ; esp += stack area base address (ESP common) + (STM_STACK_SIZE * StackOffset)
 
   ;
   ; Jump to C code
@@ -110,7 +176,7 @@ GoGetResources:
   push rdi
   push rsi
   push rbp
-  push rsp ; should be rsp ; TODO: Print the rsp here and check the dump
+  push rbp ; should be rsp
   push rbx
   push rdx
   push rcx
